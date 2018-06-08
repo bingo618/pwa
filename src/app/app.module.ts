6e8adde5a4e89b574e8e02532634dea1eedf3463
bingo618 <bin9850@163.com>
2018-06-08 16:38:49 +0800

    新增配置文件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
+import {NgModule} from '@angular/core';
 
+import {registerLocaleData} from '@angular/common';
+import zh from '@angular/common/locales/zh';
 
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
+registerLocaleData(zh);
 
-import { environment } from '../environments/environment';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {AppComponent} from './app.component';
+
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -13,9 +21,14 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot(),
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
